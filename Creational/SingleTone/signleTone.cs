630a184c02c1e31eafbdea6a40f0cc1bb5cb4eb1
Kian D. Rad <davoudi.rk@gmail.com>
2018-11-12 13:34:15 -0800

    adding signle tone as my own effort, re-clustring the structure of the project

---
+++ Creational/SingleTone/signleTone.cs
@@ -0,0 +1,20 @@
+public static sealed class singleTone{
+    private static singleTone instance = NULL;
+    private static object lockThis = NULL;
+    private singleTone(){};
+    public static singleTone getInstanceEager(){
+        this.instance = new singleTone();
+            return this.instance;
+    }
+    public static singleTone getInstanceLazy(){
+        if (instance == NULL) this.instance = new singleTone();
+        get{return this.instance;}
+    }
+    public static singleTone getInstanceThreadSafe(){
+     lock (lockThis)
+     {
+        if (instance == NULL) this.instance = new singleTone();
+        return this.instance;
+     }
+    }
+}
